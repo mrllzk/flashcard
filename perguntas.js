9addb6d3325fc5e6e907c45ebbd5fb74afd34f54
mrllzk <murillo.silva.pereira@escola.pr.gov.br>
2025-07-04 19:18:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Política',
+    'Quem é Jair Bolsonaro?',
+    'Jair Bolsonaro é um político brasileiro que foi presidente do Brasil de 2019 a 2022.'
+)
+
+criaCartao(
+    'Política',
+    'De qual partido Jair Bolsonaro foi eleito presidente?',
+    'Jair Bolsonaro foi eleito presidente pelo Partido Social Liberal (PSL) em 2018.'
+)
+
+criaCartao(
+    'Política',
+    'Qual era a profissão de Jair Bolsonaro antes da política?',
+    'Antes da política, Jair Bolsonaro era militar do Exército Brasileiro.'
+)
+
+criaCartao(
+    'Política',
+    'Em que ano Jair Bolsonaro foi eleito presidente?',
+    'Jair Bolsonaro foi eleito presidente do Brasil em 2018.'
+)
